6aee52176bcb7fb70fc4907b91994da2ba56e0b8
Jake Jarrett <jakeryanjarrett@gmail.com>
2024-04-13 01:14:25 +1000

    Init

---
+++ packages/checkout/src/app/app.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <p>Hello</p>
+    </StyledApp>
+  );
+}
+
+export default App;
---
+++ packages/product/src/app/app.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <p>Product</p>
+    </StyledApp>
+  );
+}
+
+export default App;
